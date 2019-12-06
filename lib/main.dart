4f897ef2a18368d5cb5eca8fa1a12c3da53cf294
Georg Jaehnig <georg@jaehnig.org>
2019-12-06 18:32:55 +0300

    Add settings window.

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,9 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.settings), onPressed: _settings),
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -92,6 +95,20 @@ class _MyHomePageState extends State<MyHomePage> {
             )
           ],
         ),
+      ),
+    );
+  }
+
+  void _settings() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Settings'),
+            ),
+          );
+        },
       ),
     );
   }
