6033e62292a4c75e003595cbb7bcf400871b7df8
Georg Jaehnig <georg@jaehnig.org>
2019-12-06 19:10:38 +0300

    Remove comments.

--- lib/main.dart
+++ lib/main.dart
@@ -42,16 +42,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         actions: <Widget>[
           IconButton(icon: Icon(Icons.settings), onPressed: _settings),
         ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
